8468e708b6bdd58040e83237f252138be03baf30
srinivasasai chinnari <chinnarichekravarthi@gmail.com>
2026-01-21 23:02:02 +0530

    listen issue solved

--- backend/src/server.js
+++ backend/src/server.js
@@ -101,9 +101,6 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
-
-server.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
-});
+  
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("Server running"));
